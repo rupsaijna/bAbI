eae26026e4e79f610cafeb6c2cfeffdf8acc77d0
Rupsa Saha <rupsajina@gmail.com>
2019-08-26 15:21:25 +0200

    Create preproc.py

---
+++ preproc.py
@@ -0,0 +1,52 @@
+def tokenize(sent):
+    '''
+    argument: a sentence string
+    returns a list of tokens(words)
+    '''
+    return [ x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
+ 
+def parse_stories(lines):
+    '''
+    - Parse stories provided in the bAbI tasks format
+    - A story starts from line 1 to line 15. Every 3rd line,
+      there is a question &amp;amp;amp;amp;amp; answer.
+    - Function extracts sub-stories within a story and
+      creates tuples
+    '''
+    data = []
+    story = []
+    for line in lines:
+        line = line.decode('utf-8').strip()
+        nid, line = line.split(' ', 1)
+        nid = int(nid)
+        if nid == 1:
+            # reset story when line ID=1 (start of new story)
+            story = []
+        if '\t' in line:
+            # this line is tab separated Q, A &amp;amp;amp;amp;amp; support fact ID
+            q, a, supporting = line.split('\t')
+            # tokenize the words of question
+            q = tokenize(q)
+            # Provide all the sub-stories till this question
+            substory = [x for x in story if x]
+            # A story ends and is appended to global story data-set
+            data.append((substory, q, a))
+            story.append('')
+        else:
+            # this line is a sentence of story
+            sent = tokenize(line)
+            story.append(sent)
+    return data
+ 
+def get_stories(f):
+    '''
+    argument: filename
+    returns list of all stories in the argument data-set file
+    '''
+    # read the data file and parse 10k stories
+    data = parse_stories(f.readlines())
+    # lambda func to flatten the list of sentences into one list
+    flatten = lambda data: reduce(lambda x, y: x + y, data)
+    # creating list of tuples for each story
+    data = [(flatten(story), q, answer) for story, q, answer in data]
+    return data
