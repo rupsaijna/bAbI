515262f163dba3183ea7840cfcb8c1053fa36503
rupsaijna <rupsajina@gmail.com>
2019-08-27 10:46:37 +0000

    code

--- code/preproc.py
+++ code/preproc.py
@@ -1,3 +1,8 @@
+import re
+from functools import reduce
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
 def tokenize(sent):
     '''
     argument: a sentence string
@@ -16,7 +21,7 @@ def parse_stories(lines):
     data = []
     story = []
     for line in lines:
-        line = line.decode('utf-8').strip()
+        line = line.strip() #line.decode('utf-8').strip()
         nid, line = line.split(' ', 1)
         nid = int(nid)
         if nid == 1:
@@ -43,6 +48,7 @@ def get_stories(f):
     argument: filename
     returns list of all stories in the argument data-set file
     '''
+    f=open(f,'r')
     # read the data file and parse 10k stories
     data = parse_stories(f.readlines())
     # lambda func to flatten the list of sentences into one list
@@ -50,3 +56,26 @@ def get_stories(f):
     # creating list of tuples for each story
     data = [(flatten(story), q, answer) for story, q, answer in data]
     return data
+
+
+def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
+    # story vector initialization
+    X = []
+    # query vector initialization
+    Xq = []
+    # answer vector intialization
+    Y = []
+    for story, query, answer in data:
+        # creating list of story word indices
+        x = [word_idx[w] for w in story]
+        # creating list of query word indices
+        xq = [word_idx[w] for w in query]
+        # let's not forget that index 0 is reserved
+        y = np.zeros(len(word_idx) + 1)
+        # creating label 1 for the answer word index
+        y[word_idx[answer]] = 1
+        X.append(x)
+        Xq.append(xq)
+        Y.append(y)
+    return (pad_sequences(X, maxlen=story_maxlen),
+            pad_sequences(Xq, maxlen=query_maxlen), np.array(Y))
--- code/single_supporting_fact.py
+++ code/single_supporting_fact.py
@@ -1,11 +1,78 @@
+from preproc import *
 
-challenge = 'tasks_1-20_v1-2/en-10k/qa1_single-supporting-fact_{}.txt'
-print('Extracting stories for the challenge: single_supporting_fact_10k')
+challenge = '../data/tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
+print('Extracting stories for the challenge: single_supporting_fact')
+
 # Extracting train stories
-train_stories = get_stories(tar.extractfile(challenge.format('train')))
+train_stories = get_stories(challenge.format('train'))
 # Extracting test stories
-test_stories = get_stories(tar.extractfile(challenge.format('test')))
+test_stories = get_stories(challenge.format('test'))
 
 print('Number of training stories:', len(train_stories))
 print('Number of test stories:', len(test_stories))
-print(train_stories[0])
+print('Story 1:', train_stories[0])
+
+
+# creating vocabulary of words in train and test set
+vocab = set()
+for story, q, answer in train_stories + test_stories:
+    vocab |= set(story + q + [answer])
+ 
+# sorting the vocabulary
+vocab = sorted(vocab)
+ 
+# Reserve 0 for masking via pad_sequences
+vocab_size = len(vocab) + 1
+ 
+# calculate maximum length of story
+story_maxlen = max(map(len, (x for x, _, _ in train_stories + test_stories)))
+ 
+# calculate maximum length of question/query
+query_maxlen = max(map(len, (x for _, x, _ in train_stories + test_stories)))
+ 
+# creating word to index dictionary
+word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+ 
+# creating index to word dictionary
+idx_word = dict((i+1, c) for i,c in enumerate(vocab))
+ 
+# vectorize train story, query and answer sentences/word using vocab
+inputs_train, queries_train, answers_train = vectorize_stories(train_stories,
+                                                               word_idx,
+                                                               story_maxlen,
+                                                               query_maxlen)
+# vectorize test story, query and answer sentences/word using vocab
+inputs_test, queries_test, answers_test = vectorize_stories(test_stories,
+                                                            word_idx,
+                                                            story_maxlen,
+                                                            query_maxlen)
+
+print('-------------------------')
+print('Vocabulary:\n',vocab,"\n")
+print('Vocab size:', vocab_size, 'unique words')
+print('Story max length:', story_maxlen, 'words')
+print('Query max length:', query_maxlen, 'words')
+print('Number of training stories:', len(train_stories))
+print('Number of test stories:', len(test_stories))
+print('-------------------------')
+
+print('-------------------------')
+print('inputs: integer tensor of shape (samples, max_length)')
+print('inputs_train shape:', inputs_train.shape)
+print('inputs_test shape:', inputs_test.shape)
+print('input train sample', inputs_train[0,:])
+print('-------------------------')
+
+print('-------------------------')
+print('queries: integer tensor of shape (samples, max_length)')
+print('queries_train shape:', queries_train.shape)
+print('queries_test shape:', queries_test.shape)
+print('query train sample', queries_train[0,:])
+print('-------------------------')
+
+print('-------------------------')
+print('answers: binary (1 or 0) tensor of shape (samples, vocab_size)')
+print('answers_train shape:', answers_train.shape)
+print('answers_test shape:', answers_test.shape)
+print('answer train sample', answers_train[0,:])
+print('-------------------------')
