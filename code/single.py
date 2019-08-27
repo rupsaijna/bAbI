55d93c1cb9bb7e315d00d99569b57e5ae8501f92
Rupsa Saha <rupsajina@gmail.com>
2019-08-27 18:52:46 +0200

    Create single.py

---
+++ code/single.py
@@ -0,0 +1,66 @@
+from preproc import *
+
+challenge = '../data/tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
+print('Extracting stories for the challenge: single_supporting_fact')
+
+# Extracting stories
+train_stories = get_stories(challenge.format('train'))
+test_stories = get_stories(challenge.format('test'))
+
+print('Number of training stories:', len(train_stories))
+print('Number of test stories:', len(test_stories))
+
+# creating vocabulary of words in train and test set
+vocab = set()
+for story, q, answer in train_stories + test_stories:
+    vocab |= set(story + q + [answer])
+ 
+# sorting the vocabulary
+vocab = sorted(vocab)
+ 
+# Reserve 0 for masking via pad_sequences
+vocab_size = len(vocab) + 1
+ 
+# calculate maximum length of story
+story_maxlen = max(map(len, (x for x, _, _ in train_stories + test_stories)))
+ 
+# calculate maximum length of question/query
+query_maxlen = max(map(len, (x for _, x, _ in train_stories + test_stories)))
+ 
+# creating word to index dictionary
+word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+ 
+# creating index to word dictionary
+idx_word = dict((i+1, c) for i,c in enumerate(vocab))
+ 
+# vectorize train story, query and answer sentences/word using vocab
+inputs_train, queries_train, answers_train = vectorize_stories(train_stories,
+                                                               word_idx,
+                                                               story_maxlen,
+                                                               query_maxlen)
+# vectorize test story, query and answer sentences/word using vocab
+inputs_test, queries_test, answers_test = vectorize_stories(test_stories,
+                                                            word_idx,
+                                                            story_maxlen,
+                                                            query_maxlen)
+
+print('-------------------------')
+print('Vocabulary:\n',vocab,"\n")
+print('Vocab size:', vocab_size, 'unique words')
+print('Story max length:', story_maxlen, 'words')
+print('Query max length:', query_maxlen, 'words')
+print('Number of training stories:', len(train_stories))
+print('Number of test stories:', len(test_stories))
+print('-------------------------')
+
+bitlength=story_maxlen.bit_length()
+new_inputs_train=[]
+for inp in inputs_train:
+  ip=[]
+  for w in inp:
+    t=list(bin(int(w))[2:].zfill(bitlength))
+    ip+=t
+  new_inputs_train.append(ip)
+  
+np.asarray(new_inputs_train)
+print(new_inputs_train.shape)
