936b15a7ade669c01d7118ed917625e1fb90da58
rupsaijna <rupsajina@gmail.com>
2019-09-09 11:37:06 +0000

    comit

--- code/basictm_single_supporting_fact.py
+++ code/basictm_single_supporting_fact.py
@@ -1,3 +1,4 @@
+from sklearn.metrics.pairwise import cosine_similarity
 from preproc import *
 from pyTsetlinMachine.tm import MultiClassTsetlinMachine
 
@@ -35,12 +36,12 @@ word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
 idx_word = dict((i+1, c) for i,c in enumerate(vocab))
  
 # vectorize train story, query and answer sentences/word using vocab
-inputs_train, queries_train, answers_train = vectorize_stories(train_stories,
+inputs_train, queries_train, answers_train, queries_train_padded = vectorize_stories(train_stories,
                                                                word_idx,
                                                                story_maxlen,
                                                                query_maxlen)
 # vectorize test story, query and answer sentences/word using vocab
-inputs_test, queries_test, answers_test = vectorize_stories(test_stories,
+inputs_test, queries_test, answers_test, queries_test_padded = vectorize_stories(test_stories,
                                                             word_idx,
                                                             story_maxlen,
                                                             query_maxlen)
@@ -55,6 +56,7 @@ print('Number of test stories:', len(test_stories))
 print('-------------------------')
 
 bitlength=story_maxlen.bit_length()
+
 new_inputs_train=[]
 for inp in inputs_train:
   ip=[]
@@ -76,38 +78,54 @@ for inp in inputs_test:
   
 new_inputs_test= np.asarray(new_inputs_test)
 
-bitlength=query_maxlen.bit_length()
 new_queries_train=[]
-for inp in queries_train:
+for inp in queries_train_padded:
   ip=[]
   for w in inp:
     t=list(bin(int(w))[2:].zfill(bitlength))
     ip+=t
   ip=[int(t) for t in ip]
   new_queries_train.append(ip)
-
 new_queries_train= np.asarray(new_queries_train)
 
 new_queries_test=[]
-for inp in queries_test:
+for inp in queries_test_padded:
   ip=[]
   for w in inp:
     t=list(bin(int(w))[2:].zfill(bitlength))
     ip+=t
   ip=[int(t) for t in ip]
   new_queries_test.append(ip)
-
 new_queries_test= np.asarray(new_queries_test)
-
 match_train = []
-for q in new_queries_train:
-    for s in new_inputs_train:
-        m=np.dot([input_encoded_m, question_encoded], axes=-1, normalize=False)
-        match_train.append(m)
+for q in queries_train_padded:
+    match_q=[]
+    for s in inputs_train:
+        #m=np.dot(s, q)
+        m=cosine_similarity([s,q])
+        match_q.append(m[0,1])
+    print(match_q)
+    print(max(match_q), match_q.count(max(match_q)))
+    kflg
+    match_train.append(match_q)
 match_train=np.asarray(match_train)
 print(match_train.shape)
 
+
 '''
+match_train = []
+for q in new_queries_train:
+    match_q=[]
+    for s in new_inputs_train:
+        m=np.dot(s, q)
+        print(s,q,m)
+        match_q.append(m)
+        print(match_q)
+        kkllk
+    match_train.append(match_q)
+match_train=np.asarray(match_train)
+print(match_train.shape)
+
 tm = MultiClassTsetlinMachine(800, 40, 5.0)
 tm.fit(new_inputs_train, new_queries_train, epochs=1)
 tm_results=tm.predict(X_test) == Y_test).mean()
