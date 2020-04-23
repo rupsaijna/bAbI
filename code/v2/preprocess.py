c5993a2e70f1d0774f53e3b66c201484191c43d5
Rupsa Saha <rupsajina@gmail.com>
2020-04-23 16:55:30 +0200

    Update preprocess.py

--- code/v2/preprocess.py
+++ code/v2/preprocess.py
@@ -32,7 +32,7 @@ def parse_stories(lines):
             story.append('')
         else:
             # this line is a sentence of story
-            sent = tokenize(line)
+            sent=line #sent = tokenize(line)
             story.append(sent)
     return data
  
