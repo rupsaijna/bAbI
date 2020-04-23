daafe79ea3bfe521f4299a45c567904cb5f9ef8e
Rupsa Saha <rupsajina@gmail.com>
2020-04-23 16:53:31 +0200

    Update explore_single_supporting_fact.py

--- code/v2/explore_single_supporting_fact.py
+++ code/v2/explore_single_supporting_fact.py
@@ -1,6 +1,6 @@
 from preprocess import *
 
-challenge = '../data/tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
+challenge = '../../data/tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
 print('Extracting stories for the challenge: single_supporting_fact')
 
 
