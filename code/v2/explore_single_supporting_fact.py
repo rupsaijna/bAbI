db00783716c3f974f9164704f67acd26988976c5
Rupsa Saha <rupsajina@gmail.com>
2020-04-24 13:55:02 +0200

    Update explore_single_supporting_fact.py

--- code/v2/explore_single_supporting_fact.py
+++ code/v2/explore_single_supporting_fact.py
@@ -4,7 +4,7 @@ EMBEDDING_DIM = 100
 challenge = '../../data/tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
 print('Extracting stories for the challenge: single_supporting_fact')
 
-glove_embeddings=load_meanbinarized_glove(EMBEDDING_DIM) #load_binarized_glove()
+#glove_embeddings=load_meanbinarized_glove(EMBEDDING_DIM) #load_binarized_glove()
 
 
 # Extracting train stories
@@ -19,7 +19,7 @@ train_features=[]
 for trs in train_stories:
 	print('Story:', trs)
 	#temp=story_to_glove_features(trs, glove_embeddings, EMBEDDING_DIM)
-	temp=story_to_glove_features(trs,context_length=2)
+	temp=story_to_gram_features(trs,context_length=2)
 	train_features+=temp
 	break
 
