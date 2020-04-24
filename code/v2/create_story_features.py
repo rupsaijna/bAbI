61ca13cf9b97089b737d0e2467f704ed8ed11efe
Rupsa Saha <rupsajina@gmail.com>
2020-04-24 14:32:17 +0200

    Update create_story_features.py

--- code/v2/create_story_features.py
+++ code/v2/create_story_features.py
@@ -2,7 +2,7 @@ from preprocess2 import *
 import pandas as pd
 
 EMBEDDING_DIM = 100
-context_length=2
+CONTEXT_LENGTH=2
 
 challenge = '../../data/tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
 print('Extracting stories for the challenge: single_supporting_fact')
@@ -10,12 +10,9 @@ print('Extracting stories for the challenge: single_supporting_fact')
 
 #glove_embeddings=load_meanbinarized_glove(EMBEDDING_DIM) #load_binarized_glove()
 
-gram_headers=create_features_headers(headertype=1,context_length=2)
-glove_headers=create_features_headers(headertype=2,embeddingdim=EMBEDDING_DIM,context_length=2)
+gram_headers=create_features_headers(headertype=1,context_length=CONTEXT_LENGTH)
+glove_headers=create_features_headers(headertype=2,embeddingdim=EMBEDDING_DIM,context_length=CONTEXT_LENGTH)
 
-print(gram_headers)
-print(len(gram_headers), len(glove_headers))
-dsa
 ftype='train'
 # Extracting train stories
 train_stories = get_stories(challenge.format(ftype))
@@ -25,8 +22,8 @@ train_features_glove=[]
 counter=1
 for trs in train_stories:
 	print('Train Story:',counter)
-	train_features_gram+=story_to_gram_features(trs,context_length=2)
-	train_features_glove+=story_to_glove_features(trs,glove_embeddings, EMBEDDING_DIM,context_length=2)
+	train_features_gram+=story_to_gram_features(trs,context_length=CONTEXT_LENGTH)
+	train_features_glove+=story_to_glove_features(trs,glove_embeddings, EMBEDDING_DIM,context_length=CONTEXT_LENGTH)
 	counter+=1
 	
 train_features_gram=pd.DataFrame(train_features_gram, columns=gram_headers)
@@ -34,7 +31,7 @@ train_features_glove=pd.DataFrame(train_features_glove, columns=glove_headers)
 print(train_features_gram.shape)
 print(train_features_glove.shape)
 
-savename='../../pickles/spacy/nonbinarized_features_'+ftype
+savename='../../pickles/spacy/nonbinarized_features_context'+str(CONTEXT_LENGTH)+'_'+ftype
 train_features_gram.to_pickle(savename+'_gram.pkl')
 print('Saved: ',savename+'_gram.pkl')
 
@@ -52,8 +49,8 @@ test_features_glove=[]
 counter=1
 for trs in test_stories:
 	print('Test Story:',counter)
-	test_features_gram+=story_to_gram_features(trs)
-	test_features_glove+=story_to_glove_features(trs,glove_embeddings, EMBEDDING_DIM)
+	test_features_gram+=story_to_gram_features(trs,context_length=CONTEXT_LENGTH)
+	test_features_glove+=story_to_glove_features(trs,glove_embeddings, EMBEDDING_DIM,context_length=CONTEXT_LENGTH)
 	counter+=1
   
 test_features_gram=pd.DataFrame(test_features_gram, columns=gram_headers)
@@ -61,7 +58,7 @@ print(test_features_gram.shape)
 test_features_glove=pd.DataFrame(test_features_glove, columns=glove_headers)
 print(test_features_glove.shape)
 
-savename='../../pickles/spacy/nonbinarized_features_'+ftype
+savename='../../pickles/spacy/nonbinarized_features_context'+str(CONTEXT_LENGTH)+'_'+ftype
 test_features_gram.to_pickle(savename+'_gram.pkl')
 print('Saved: ',savename+'_gram.pkl')
 
