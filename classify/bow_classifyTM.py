0b8534b0306806eb15ff92d95db71ffc278b4ade
Rupsa Saha <rupsajina@gmail.com>
2020-05-19 14:15:44 +0200

    Update bow_classifyTM.py

--- classify/bow_classifyTM.py
+++ classify/bow_classifyTM.py
@@ -43,6 +43,7 @@ y=[int(yy) for yy in y]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42, shuffle=True)
 
+print(featureheaderset)
 print(len(featureheaderset))
 print(X_train.shape)
 df
