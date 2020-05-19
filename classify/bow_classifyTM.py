76531765851615045c2ffffc244038f757b7b83a
Rupsa Saha <rupsajina@gmail.com>
2020-05-19 10:10:05 +0200

    Update bow_classifyTM.py

--- classify/bow_classifyTM.py
+++ classify/bow_classifyTM.py
@@ -50,7 +50,7 @@ for i in range(RUNS):
 	for ls in range(len(labels_set)):
 		print(labels_set[ls]+' : '+str(round(prf_test_class[0][ls],2))+', '+str(round(prf_test_class[1][ls],2))+', '+str(round(prf_test_class[2][ls],2))+', '+str(round(prf_test_class[3][ls],2)))
 
-print('Over '+str(RUNS)+' runs: 'str(np.mean(allacc, axis=0))+' +/- '+str(np.std(allacc, axis=0)))
+print('Over '+str(RUNS)+' runs: '+str(np.mean(allacc, axis=0))+' +/- '+str(np.std(allacc, axis=0)))
 
 lastruns=int(RUNS/3)
 print('Last '+str(lastruns)+' runs: '+str(np.mean(allacc[-lastruns:], axis=0))+' +/- '+str(np.std(allacc[-lastruns:], axis=0)))
