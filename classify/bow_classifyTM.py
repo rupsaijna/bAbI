67b852ba94224e2a3c67987931eb00a7bce62387
Rupsa Saha <rupsajina@gmail.com>
2020-05-19 15:41:12 +0200

    Update bow_classifyTM.py

--- classify/bow_classifyTM.py
+++ classify/bow_classifyTM.py
@@ -92,7 +92,7 @@ for cur_cls in range(len(labels_set)):
 				this_clause+=featureheaderset[f]+';'
 			if action_negated==1:
 				this_clause+=' #'+featureheaderset[f]+';'
-		this_clause+='\t'+clause_type+'\t'+str(cur_cls)	
+		this_clause+='\t'+clause_type+'\t'+str(labels_set[cur_cls])	
 		fout_c.write(str(this_clause)+'\n')
 fout_c.close()
 
