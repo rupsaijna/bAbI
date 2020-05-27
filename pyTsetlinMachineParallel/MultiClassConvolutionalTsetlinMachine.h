459f8074aecec6a112de69312e85c2a2c2397a11
Rupsa Saha <rupsajina@gmail.com>
2020-05-27 18:23:07 +0200

    Update MultiClassConvolutionalTsetlinMachine.h

--- pyTsetlinMachineParallel/MultiClassConvolutionalTsetlinMachine.h
+++ pyTsetlinMachineParallel/MultiClassConvolutionalTsetlinMachine.h
@@ -62,3 +62,5 @@ void mc_tm_transform(struct MultiClassTsetlinMachine *mc_tm, unsigned int *X,  u
 
 void mc_tm_clause_configuration(struct MultiClassTsetlinMachine *mc_tm, int class, int clause, unsigned int *clause_configuration);
 
+
+void mc_tm_predict_clauseprint(struct MultiClassTsetlinMachine *mc_tm, unsigned int *X, int *y, int number_of_examples); /* Print local clauses --RUPSA*/
