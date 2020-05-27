fa3eaada036d316ce51211d21367581c838889cc
Rupsa Saha <rupsajina@gmail.com>
2020-05-27 18:20:23 +0200

    Update ConvolutionalTsetlinMachine.h

--- pyTsetlinMachineParallel/ConvolutionalTsetlinMachine.h
+++ pyTsetlinMachineParallel/ConvolutionalTsetlinMachine.h
@@ -99,3 +99,6 @@ void tm_get_clause_weights(struct TsetlinMachine *tm, unsigned int *clause_weigh
 void tm_set_clause_weights(struct TsetlinMachine *tm, unsigned int *clause_weights);
 
 
+int tm_score_printclause(struct TsetlinMachine *tm, unsigned int *Xi); /*for printing local clauses*/
+
+
