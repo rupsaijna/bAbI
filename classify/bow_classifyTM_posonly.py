5f07e03faa80edc58be3fa8cfecf335cc846c120
Rupsa Saha <rupsajina@gmail.com>
2020-05-28 13:40:47 +0200

    Update bow_classifyTM_posonly.py

--- classify/bow_classifyTM_posonly.py
+++ classify/bow_classifyTM_posonly.py
@@ -22,9 +22,9 @@ for l in lines[3:]:
 		featureheaderset=lt[2].replace('\n','').split(',')
 
 if 'sentenceleveltransform' not in fname:
-	CLAUSES=33
-	T=35
-	s=12.5
+	CLAUSES=22
+	T=19
+	s=9.5
 	weighting = True
 	training_epoch=5
 	RUNS=100
