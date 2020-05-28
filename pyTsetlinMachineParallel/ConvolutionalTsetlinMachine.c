ca70525cb04650be170597efd120972c17f1ea15
Rupsa Saha <rupsajina@gmail.com>
2020-05-28 09:20:53 +0200

    Update ConvolutionalTsetlinMachine.c

--- pyTsetlinMachineParallel/ConvolutionalTsetlinMachine.c
+++ pyTsetlinMachineParallel/ConvolutionalTsetlinMachine.c
@@ -219,9 +219,9 @@ static inline int sum_up_class_votes_print(struct TsetlinMachine *tm, int classn
 
 		int temp=0;
 		temp= tm->clause_weights[j] * ((tm->clause_output[clause_chunk] & (1 << clause_pos)) > 0);
-		printf("%d ",temp);
+		//printf("%d ",temp);
 		if (temp!=0){
-			printf("%s%d%s",":",j," ");
+			//printf("%s%d%s",":",j," ");
 			fprintf(f, "%s%d","e",examplenum);
 			fprintf(f, "%s%d"," c",classnum);
 			fprintf(f, "%s%d"," CL",j);
