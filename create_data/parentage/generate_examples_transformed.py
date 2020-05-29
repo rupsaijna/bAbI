3b2d29fc7b37e2dbdf892e9abff8ddbee1680f58
Rupsa Saha <rupsajina@gmail.com>
2020-05-29 08:41:43 +0200

    Update generate_examples_transformed.py

--- create_data/parentage/generate_examples_transformed.py
+++ create_data/parentage/generate_examples_transformed.py
@@ -69,7 +69,7 @@ def generate(num_sentences,names,num_examples):
 		
 		###
 		names=org_names
-		printnames()
+		print(names)
 		temp_names_first=random.sample(names, k=2)
 		temp_example=temp_names_first[0]+' is the parent of '+temp_names_first[1]+'. '
 		temp_example_transformed='X is the parent of Y. '
