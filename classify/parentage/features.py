e1c5e5c85e5f7f362ac24cc7fc4390089c5b451f
Rupsa Saha <rupsajina@gmail.com>
2020-05-29 09:21:15 +0200

    Update features.py

--- classify/parentage/features.py
+++ classify/parentage/features.py
@@ -7,14 +7,10 @@ f=open(fname,'r')
 data=f.readlines()
 f.close()
 
-if 'sentenceleveltransform' not in fname:
-	f=open(fname.replace('.txt','_meta.txt'),'r')
-	labels_set=f.readlines()[1].replace('\n','').split(',')
-	f.close()
-	labels_set=[ls.replace('the ','') for ls in labels_set]
-else:
-	labels_set=[]
-	
+f=open(fname.replace('.txt','_meta.txt'),'r')
+labels_set=f.readlines()[1].replace('\n','').split(',')
+f.close()
+
 data=[d.replace('\n','') for d in data]
 
 data=[d.split('\t') for d in data]
@@ -23,54 +19,42 @@ text=[d[0] for d in data]
 labels=[d[1] for d in data]
 #labels_set=list(set(labels))
 
-word_set_sentences= set()
-word_set_questions= set()
+realtionships_set= set()
 newtext=[]
 for t in text:
-	sent=t.split('.')
-	sent=[s.replace('.','').replace('?','').strip() for s in sent]
-	sent=[s.split(' ') for s in sent]
-	newtext.append(sent)
-	tempsent=[word for subsent in sent[:-1] for word in subsent]
-	for w in tempsent:
-		word_set_sentences.add(w)
-	for w in sent[-1]:
-		word_set_questions.add(w)
+	sent=t.replace('\n','').split('.')
+	for s in sent[:-1]:
+		s=s.strip().replace('.','').split(' ')
+		temp_rel=s[0]+'_'+s[-1]
+		realtionships_set.add(temp_rel)
+	
 
-word_set_sentences=list(word_set_sentences)
-word_set_questions=list(word_set_questions)
-numsentences=len(newtext[0])-1
+realtionships_set=list(realtionships_set)
+numsentences=2
 
-if 'sentenceleveltransform' in fname:
-	for nn in range(1,numsentences+1):
-		labels_set.append('LOC'+str(nn))
-
-featureset=np.zeros((len(newtext),len(word_set_sentences)*numsentences+len(word_set_questions)+1))
+featureset=np.zeros((len(newtext),len(realtionships_set)))
 
 sentfeaturelen=len(word_set_sentences)
 qsfeaturelen=len(word_set_questions)
 
 
-print(word_set_sentences)
-print(word_set_questions)
+print(realtionships_set)
 print(labels_set)
 featureheaders=[]
-for n in range(1,numsentences+1):
-	featureheaders+=['s_'+str(n)+'_'+w for w in word_set_sentences]
-featureheaders+=['q_'+w for w in word_set_questions]
-
+for n in realtionships_set:
+	featureheaders+='isParent('+str(n)+')'
 textind=0
-for nt in newtext:
-	startind=0
-	for sentence in nt[:-1]:
-		tempfeature=[1 if word_set_sentences[i] in sentence else 0 for i in range(sentfeaturelen)]
-		featureset[textind,startind:startind+sentfeaturelen]=tempfeature
-		startind=startind+sentfeaturelen
-	tempfeature=[1 if word_set_questions[i] in nt[-1] else 0 for i in range(qsfeaturelen) ]
-	featureset[textind,startind:-1]=tempfeature
-	featureset[textind,-1]=labels_set.index(labels[textind])
+for t in text:
+	sent=t.replace('\n','').split('.')
+	temp_feature=np.zero(len(realtionships_set))
+	for s in sent[:-1]:
+		s=s.strip().replace('.','').split(' ')
+		temp_rel=s[0]+'_'+s[-1]
+		ind=realtionships_set.index(temp_rel)
+		temp_feature[ind]=1
+	featureset[textind]=temp_feature
 	textind+=1
-
+		
 	
 #print(featureset.shape, featureheaders, numsentences)	
 
