4df72b2a5548b29f701f21a0353a239bc8256a8f
Rupsa Saha <rupsajina@gmail.com>
2020-05-29 09:59:21 +0200

    Update classifyTM.py

--- classify/parentage/classifyTM.py
+++ classify/parentage/classifyTM.py
@@ -50,7 +50,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random
 
 print(list(y_train).count(0), list(y_train).count(1), list(y_train).count(2))
 print(list(y_test).count(0), list(y_test).count(1), list(y_test).count(2))
-dF
+
 tm = MultiClassTsetlinMachine(CLAUSES, T, s, weighted_clauses=weighting,append_negated=True)
 
 allacc=[]
