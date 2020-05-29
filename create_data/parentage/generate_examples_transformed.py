5b0de17e9e8febb00e873e27af4f448edbda24ab
Rupsa Saha <rupsajina@gmail.com>
2020-05-29 10:25:01 +0200

    Update generate_examples_transformed.py

--- create_data/parentage/generate_examples_transformed.py
+++ create_data/parentage/generate_examples_transformed.py
@@ -76,20 +76,17 @@ def generate(num_sentences,names,num_examples):
 		temp_example_transformed='X is the parent of Y. '
 		temp_names.remove(temp_names_first[0])
 		temp_names.remove(temp_names_first[1])
-		rel=random.randint(0,2)
-		if rel==0:
+		for rel in range(3):
 			temp_names_second=random.sample(temp_names, k=1)
 			temp_example+=temp_names_first[1]+' is the parent of '+temp_names_second[0]+'. '
 			temp_example+='How are '+temp_names_first[0]+' and '+temp_names_second[0]+' related?'
 			temp_example_transformed+='Y is the parent of Z. '
 			temp_example_transformed+='How are X and Z related?'
-		if rel==1:
 			temp_names_second=random.sample(temp_names, k=1)
 			temp_example+=temp_names_first[0]+' is the parent of '+temp_names_second[0]+'. '
 			temp_example+='How are '+temp_names_first[0]+' and '+temp_names_second[0]+' related?'
 			temp_example_transformed+='X is the parent of Z. '
 			temp_example_transformed+='How are X and Z related?'
-		if rel==2:
 			temp_names_second=random.sample(temp_names, k=2)
 			temp_example+=temp_names_second[0]+' is the parent of '+temp_names_second[1]+'. '
 			temp_example+='How are '+temp_names_first[0]+' and '+temp_names_second[1]+' related?'
