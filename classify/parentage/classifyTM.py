9f5159b096ef81657f1aed976c840ff9c1774d3f
Rupsa Saha <rupsajina@gmail.com>
2020-05-29 10:01:39 +0200

    Update classifyTM.py

--- classify/parentage/classifyTM.py
+++ classify/parentage/classifyTM.py
@@ -23,7 +23,7 @@ for l in lines[3:]:
 		featureheaderset=lt[2].replace('\n','').split(',')
 
 if 'sentenceleveltransform' not in fname:
-	CLAUSES=200
+	CLAUSES=500
 	T=700
 	s=79.5
 	weighting = True
