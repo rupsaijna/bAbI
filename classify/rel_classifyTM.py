67ebc1cfcb6a2638dc78ef8a52f602462ff85fd8
Rupsa Saha <rupsajina@gmail.com>
2020-06-30 15:19:30 +0200

    Update rel_classifyTM.py

--- classify/rel_classifyTM.py
+++ classify/rel_classifyTM.py
@@ -150,7 +150,7 @@ print('Last '+str(lastruns)+' runs: '+str(np.mean(allacc[-lastruns:], axis=0))+'
 
 
 ####ALL CLAUSES###
-#wrote_clauses(clause_file, df_transformed_X, tm, featureheaderset, labels_set)
+wrote_clauses(clause_file, df_transformed_X, tm, featureheaderset, labels_set)
 
 ####LOCAL VIEW###
-#get_random_local_view(X_test, 5, local_clause_file, tm, clause_file)
+get_random_local_view(X_test, 5, local_clause_file, tm, clause_file)
