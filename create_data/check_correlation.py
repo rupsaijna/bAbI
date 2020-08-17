c5de41d28395210e7210d98546e3be314837520f
Rupsa Saha <rupsajina@gmail.com>
2020-08-17 11:45:25 +0200

    Update check_correlation.py

--- create_data/check_correlation.py
+++ create_data/check_correlation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-fname=sys.argv[1]
-
+#fname=sys.argv[1]
+fname = '../generated/generated2.txt'
 df_transformed = pd.read_pickle(fname.replace('.txt','')+'_transformed_relationalfeatureset.pkl')
 
 df_transformed_X= df_transformed.drop(columns=['ANSWER'])
@@ -12,3 +12,11 @@ correl = df_transformed_X.corr()
 
 plt.matshow(correl)
 plt.savefig('plot.png')
+
+
+#############
+import seaborn as sns
+sns.set(font_scale =0.5)
+sp = sns.heatmap(correl, xticklabels=correl.columns, yticklabels=correl.columns)
+fig = sp.get_figure()
+fig.savefig("plot.png")
